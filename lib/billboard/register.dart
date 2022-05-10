3868e95b6fd116a02d78c96dadf994cc0ecb3207
Daniel-Sogbey <mathematics06physics@gmail.com>
2022-05-10 23:13:41 +0000

    updates

--- lib/billboard/register.dart
+++ lib/billboard/register.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:bilbo/home/home.dart';
 import 'package:bilbo/login/login.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class RegisterBillboard extends StatefulWidget {
   RegisterBillboard({Key? key}) : super(key: key);
@@ -12,17 +19,119 @@ class RegisterBillboard extends StatefulWidget {
 }
 
 class _RegisterBillboardState extends State<RegisterBillboard> {
-  final _nameOfOrganization = TextEditingController();
-  final _location = TextEditingController();
-  final _registrationDate = TextEditingController();
+  final qrKey = GlobalKey();
+  final _nameOfOrganizationController = TextEditingController();
+  final _locationController = TextEditingController();
+  final _registrationDateController = TextEditingController();
   String _billBoardInfo = "";
 
-  _submitBillboardInfo() {
+  _validateFields() {
+    if (_nameOfOrganizationController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please enter the name of the organization'),
+        ),
+      );
+      return;
+    } else if (_locationController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please enter the location of the organization'),
+        ),
+      );
+      return;
+    } else if (_registrationDateController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content:
+              Text('Please enter the registration date of the organization'),
+        ),
+      );
+      return;
+    } else {
+      _submitBillboardInfo(context);
+    }
+  }
+
+  _submitBillboardInfo(BuildContext context) {
     _billBoardInfo = '''{
-      "Name of organization": ${_nameOfOrganization.text};
-      "Location of billboard": ${_location.text};
-      "Registration Date": ${_registrationDate.text};
+      "Name of organization": ${_nameOfOrganizationController.text};
+      "Location of billboard": ${_locationController.text};
+      "Registration Date": ${_registrationDateController.text};
     }''';
+
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              'Notification',
+              style: TextStyle(
+                  color: Color(0xff3e3e3e),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            content: Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'You have successfully registered your billboard',
+                  ),
+                  Center(
+                    child: QrImage(
+                      key: qrKey,
+                      data: _billBoardInfo,
+                      size: 300,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              FlatButton(
+                textColor: Color(0xfffdcb03),
+                onPressed: () {
+                  _nameOfOrganizationController.text = "";
+                  _locationController.text = "";
+                  _registrationDateController.text = "";
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  "OK",
+                  style: TextStyle(
+                    color: Color(0xfffdcb03),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  void takeScreenShot() async {
+    PermissionStatus res;
+    res = await Permission.storage.request();
+    if (res.isGranted) {
+      final boundary =
+          qrKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      // We can increse the size of QR using pixel ratio
+      final image = await boundary.toImage(pixelRatio: 5.0);
+      final byteData = await (image.toByteData(format: ui.ImageByteFormat.png));
+      if (byteData != null) {
+        final pngBytes = byteData.buffer.asUint8List();
+        // getting directory of our phone
+        final directory = (await getApplicationDocumentsDirectory()).path;
+        final imgFile = File(
+          '$directory/${DateTime.now()}.png',
+        );
+        imgFile.writeAsBytes(pngBytes);
+        GallerySaver.saveImage(imgFile.path).then((success) async {
+          //In here you can show snackbar or do something in the backend at successfull download
+        });
+      }
+    }
   }
 
   @override
@@ -120,6 +229,7 @@ class _RegisterBillboardState extends State<RegisterBillboard> {
               child: Padding(
                 padding: EdgeInsets.only(left: 5, right: 5),
                 child: TextField(
+                  controller: _nameOfOrganizationController,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
@@ -150,6 +260,7 @@ class _RegisterBillboardState extends State<RegisterBillboard> {
               child: Padding(
                 padding: EdgeInsets.only(left: 5, right: 5),
                 child: TextField(
+                  controller: _locationController,
                   keyboardType: TextInputType.text,
                   decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
@@ -180,6 +291,7 @@ class _RegisterBillboardState extends State<RegisterBillboard> {
               child: Padding(
                 padding: EdgeInsets.only(left: 5, right: 5),
                 child: TextField(
+                  controller: _registrationDateController,
                   keyboardType: TextInputType.datetime,
                   decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
@@ -217,56 +329,16 @@ class _RegisterBillboardState extends State<RegisterBillboard> {
                   ),
                 ),
                 onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text(
-                            'Notification',
-                            style: TextStyle(
-                                color: Color(0xff3e3e3e),
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          content: Container(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  'You have successfully registered your billboard',
-                                ),
-                                Center(
-                                  child: QrImage(
-                                    data: _billBoardInfo,
-                                    size: 300,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          actions: [
-                            FlatButton(
-                              textColor: Color(0xfffdcb03),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text(
-                                "OK",
-                                style: TextStyle(
-                                  color: Color(0xfffdcb03),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      });
+                  _validateFields();
                 },
-                child: Text('Submit Billboard',
-                    style: TextStyle(
-                      color: Color(0xffffffff),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                    )),
+                child: Text(
+                  'Submit Billboard',
+                  style: TextStyle(
+                    color: Color(0xffffffff),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
               ),
             ),
           ],
--- lib/billboard/verify.dart
+++ lib/billboard/verify.dart
@@ -12,134 +12,122 @@ class VerifyBillboard extends StatefulWidget {
 }
 
 class _VerifyBillboardState extends State<VerifyBillboard> {
+  // String? qrCodeResult;
+  // int camera = 1;
+  // bool backCamera = true;
+
+  // Future<void> _scan() async {
+  // ScanResult codeSanner = await BarcodeScanner.scan(
+  // options: ScanOptions(
+  // useCamera: camera,
+  // ),
+  // );
+  // setState(() {
+  // qrCodeResult = codeSanner.rawContent;
+  // });
+  // }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xffffffff),
-          elevation: 0,
-          leading: Builder(
-            builder: (BuildContext context) {
-              return Container(
-                child: Icon(
-                  Icons.arrow_back_ios_rounded,
-                  color: Color(0xff056839),
-                  size: 24.0,
-                ),
-              );
-            },
-          ),
-          title: const Text('Verify Billboard',style: TextStyle(color: Color(0xff3d3c3d), fontSize: 20, fontWeight: FontWeight.w500,),),
-
-          actions: <Widget>[
-            PopupMenuButton(
-              onSelected: (result) async {
-                if(result == 0){
-                  Navigator.of(context).pop();
-                }
-
-              },
-
-              icon:const Icon(
-
-                Icons.more_vert_rounded,
+      appBar: AppBar(
+        backgroundColor: Color(0xffffffff),
+        elevation: 0,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return Container(
+              child: Icon(
+                Icons.arrow_back_ios_rounded,
                 color: Color(0xff056839),
-                size: 30.0,
-
-
+                size: 24.0,
               ),
-              offset: Offset(0, kToolbarHeight),
-
-              itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-                const PopupMenuItem(
-                  child:Padding(
-                    padding: EdgeInsets.only(left:8.0,right: 25),
-                    child: Text('logout',style: TextStyle(color: Color(0xff3e3956), fontSize: 20,fontWeight: FontWeight.w400),),
+            );
+          },
+        ),
+        title: const Text(
+          'Verify Billboard',
+          style: TextStyle(
+            color: Color(0xff3d3c3d),
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (result) async {
+              if (result == 0) {
+                Navigator.of(context).pop();
+              }
+            },
+            icon: const Icon(
+              Icons.more_vert_rounded,
+              color: Color(0xff056839),
+              size: 30.0,
+            ),
+            offset: Offset(0, kToolbarHeight),
+            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+              const PopupMenuItem(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 8.0, right: 25),
+                  child: Text(
+                    'logout',
+                    style: TextStyle(
+                        color: Color(0xff3e3956),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400),
                   ),
-                  value: 0,
                 ),
-
-              ],
+                value: 0,
+              ),
+            ],
+          ),
+          // IconButton(
+          // icon: backCamera ? Icon(Icons.camera_rear) : Icon(Icons.camera),
+          // onPressed: () {
+          // setState(() {
+          // backCamera = !backCamera;
+          // camera = backCamera ? 1 : -1;
+          // });
+          // },
+          // )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                        color: Color(0x7fbbc07),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 200.0,
+                              width: 250.0,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[],
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-
-        body: SingleChildScrollView(
-          child: Column(
-              children: <Widget>[
-
-                Container(
-                  margin: EdgeInsets.only(top:10),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Container(
-                            color: Color(0x7fbbc07),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  height: 200.0,
-                                  width: 250.0,
-
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      Container(
-                                        child: Icon(
-                                          Icons.camera_alt_rounded,
-                                          color: Color(0xff7b8b9a),
-                                          size: 40.0,
-                                        ),
-                                      ),
-                                      Container(
-                                        alignment: Alignment.center,
-                                        child: Padding(
-                                          padding: const EdgeInsets.only(top: 5,bottom:5),
-                                          child: Text(
-                                            'Upload image here...',
-                                            style: TextStyle(color: Color(
-                                                0xc6000000), fontSize: 18, fontWeight: FontWeight.w500, ),
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        alignment: Alignment.center,
-                                        child: Text(
-                                          'Max files size: 50mb',
-                                          style: TextStyle(color: Color(
-                                              0xc6adadad), fontSize: 14, fontWeight: FontWeight.w500, ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            )
-
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ]
-          ),
-        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-
-        },
+        onPressed: () {},
         backgroundColor: Color(0xfffbbc07),
         child: const Icon(Icons.qr_code_2_outlined),
       ),
-
-
-
     );
-
   }
-
 }
